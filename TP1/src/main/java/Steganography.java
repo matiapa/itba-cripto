47b762adf0b9c2b75a9e64b96d387c2fbc28135a
Matías Apablaza <matiapa@pop-os.localdomain>
2022-06-17 11:13:36 -0300

    Implemented LSB1 and LSB4 encoding and decoding

--- TP1/src/main/java/Steganography.java
+++ TP1/src/main/java/Steganography.java
@@ -1,53 +1,117 @@
+import org.apache.commons.lang3.ArrayUtils;
+
 import java.io.*;
 import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Steganography {
 
-    public static void encode(File hostFile, File payloadFile) throws IOException {
-        if(!hostFile.getName().split("\\.")[1].equals("bmp"))
+    static String FILES_DIR = "tmp/";
+
+    public static void embed(File hostFile, File payloadFile, String outFileName, EncodeMode encodeMode) throws IOException {
+        if (!hostFile.getName().split("\\.")[1].equals("bmp"))
             throw new RuntimeException("Host file must be a BMP");
+
+        int n = encodeMode== EncodeMode.LSB1 || encodeMode== EncodeMode.LSBI ? 1 : 4;
+
+        // Check that host file is big enough
+
+        String extensionName = payloadFile.getName().split("\\.")[1];
+        extensionName = String.format(".%s", extensionName);
+
+        int minSize = (int) (4 + payloadFile.length() + extensionName.length());
+        minSize *= Math.floorDiv(8, n);
+
+        if(hostFile.length() <  minSize)
+            throw new RuntimeException(String.format("Host file must have at least %d bytes", minSize));
 
         // Read host file
 
         FileInputStream is = new FileInputStream(hostFile);
-        byte[] targetBytes = new byte[(int) hostFile.length()];
-        is.read(targetBytes);
+        byte[] hostBytes = new byte[(int) hostFile.length()];
+        is.read(hostBytes);
 
-        // Write payload size
+        // Get payload metadata
 
         int payloadSize = (int) payloadFile.length();
-        byte[] srcBytes = ByteBuffer.allocate(4).putInt(payloadSize).array();
+        byte[] payloadSizeBytes = ByteBuffer.allocate(4).putInt(payloadSize).array();
 
-        int t = lsb1(srcBytes, targetBytes, 0);
+        byte[] extensionNameBytes = ArrayUtils.addAll(extensionName.getBytes(), new byte[]{0});
 
-        // Write payload data
+        // Get payload data
 
         is = new FileInputStream(payloadFile);
-        srcBytes = new byte[(int) payloadFile.length()];
-        is.read(srcBytes);
+        byte[] payloadDataBytes = new byte[payloadSize];
+        is.read(payloadDataBytes);
 
-        t = lsb1(srcBytes, targetBytes, t);
+        // Encode payload data with metadata
 
-        // Write extension name
+        byte[] extendedPayload = ArrayUtils.addAll(payloadSizeBytes, payloadDataBytes);
+        extendedPayload = ArrayUtils.addAll(extendedPayload, extensionNameBytes);
 
-        String extensionName = payloadFile.getName().split("\\.")[1];
-        srcBytes = extensionName.getBytes();
+        LSBEncode(extendedPayload, hostBytes, n);
 
-        lsb1(srcBytes, targetBytes, t);
+        // Write tampered host bytes
 
-        // Write target bytes to host file
-
-        FileOutputStream os = new FileOutputStream("out.bmp");
-        os.write(targetBytes);
+        FileOutputStream os = new FileOutputStream(FILES_DIR.concat(outFileName));
+        os.write(hostBytes);
     }
 
-    static int lsb1(byte[] srcBytes, byte[] targetBytes, int targetOffsetBits) {
-        int srcBitN=0, targetBitN=7+targetOffsetBits;
-        while(srcBitN < srcBytes.length * 8) {
-            copyBit(srcBytes, srcBitN, targetBytes, targetBitN);
-            srcBitN++; targetBitN+=8;
+    public static void extract(File hostFile, String outFileName, EncodeMode encodeMode) throws IOException {
+        if (!hostFile.getName().split("\\.")[1].equals("bmp"))
+            throw new RuntimeException("Host file must be a BMP");
+
+        int n = encodeMode== EncodeMode.LSB1 || encodeMode== EncodeMode.LSBI ? 1 : 4;
+
+        // Read host file
+
+        FileInputStream is = new FileInputStream(hostFile);
+        byte[] scrBytes = new byte[(int) hostFile.length()];
+        is.read(scrBytes);
+
+        // Decode payload data with metadata
+
+        byte[] targetBytes = new byte[(int) Math.ceil(hostFile.length() / (8.0 / n))];
+        LSBDecode(scrBytes, targetBytes, n);
+
+        // Get payload metadata
+
+        int payloadSize = ByteBuffer.wrap(targetBytes, 0, 4).getInt();
+
+        List<Byte> extensionNameBytes = new ArrayList<>();
+        for(int i=4+payloadSize; targetBytes[i] != 0; i++)
+            extensionNameBytes.add(targetBytes[i]);
+        String extensionName = new String(ArrayUtils.toPrimitive(extensionNameBytes.toArray(new Byte[0])), StandardCharsets.UTF_8);
+
+        // Write payload data into output file
+
+        String filename = String.format("%s%s%s", FILES_DIR, outFileName, extensionName);
+        File outputFile = new File(filename);
+        FileOutputStream os = new FileOutputStream(outputFile);
+
+        os.write(targetBytes, 4, payloadSize);
+    }
+
+
+    static void LSBEncode(byte[] payload, byte[] host, int n) {
+        int payloadBit=0, hostBit=8-n;
+        while(payloadBit < payload.length * 8) {
+            for(int i=hostBit; i<hostBit+n; i++)
+                copyBit(payload, payloadBit++, host, i);
+            hostBit += 8;
         }
-        return targetBitN;
+    }
+
+
+    static void LSBDecode(byte[] host, byte[] payload, int n) {
+        int payloadBit=0, hostBit=8-n;
+        while(payloadBit < payload.length * 8) {
+            for(int i=hostBit; i<hostBit+n; i++)
+                copyBit(host, i, payload, payloadBit++);
+            hostBit += 8;
+        }
     }
 
     static void copyBit(byte[] src, int srcBitN, byte[] target, int targetBitN) {
@@ -61,10 +125,22 @@ public class Steganography {
     }
 
     public static void main(String[] args) throws IOException {
-        File hostFile = new File("host.bmp");
-        File payloadFile = new File("payload.bin");
+        File hostFile = new File(FILES_DIR.concat("host.bmp"));
+        File payloadFile = new File(FILES_DIR.concat("payload.bin"));
+        embed(hostFile, payloadFile, "host_tampered.bmp", EncodeMode.LSB4);
 
-        encode(hostFile, payloadFile);
+        // TODO: Mostrar error si el tamanio del host es insuficiente
+        // TODO: Mostrar error si el host posee compresion
+        // TODO: Agregar parsing de argumentos y valores default
+
+        File hostTamperedFile = new File(FILES_DIR.concat("host_tampered.bmp"));
+        extract(hostTamperedFile, "payload_recovered", EncodeMode.LSB4);
     }
+
+    // HT: FF x31 FF  FF x8 FF FF FF FF
+    // PL: 0  x31 1   0  x8 0  0  1  0
+    // OT: FE x31 FF  FE x8 FE FE FF FE
+
+    public enum EncodeMode {LSB1, LSB4, LSBI}
 
 }
