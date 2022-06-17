f253aab23096da9b65cbe5e9009f76eb99ed91e3
Matias J. Apablaza <mapablaza@itba.edu.ar>
2022-06-17 14:48:13 -0300

    Implemented LSBI

--- TP1/src/main/java/Steganography.java
+++ TP1/src/main/java/Steganography.java
@@ -21,8 +21,12 @@ public class Steganography {
         String extensionName = payloadFile.getName().split("\\.")[1];
         extensionName = String.format(".%s", extensionName);
 
-        int minSize = (int) (4 + payloadFile.length() + extensionName.length());
+        int minSize = (int) (4 + payloadFile.length() + extensionName.length() + 1);
         minSize *= Math.floorDiv(8, n);
+        minSize += encodeMode == EncodeMode.LSBI ? 4 : 0;
+
+        System.out.println(minSize);
+        System.out.println(hostFile.length());
 
         if(hostFile.length() <  minSize)
             throw new RuntimeException(String.format("Host file must have at least %d bytes", minSize));
@@ -51,7 +55,11 @@ public class Steganography {
         byte[] extendedPayload = ArrayUtils.addAll(payloadSizeBytes, payloadDataBytes);
         extendedPayload = ArrayUtils.addAll(extendedPayload, extensionNameBytes);
 
-        LSBEncode(extendedPayload, hostBytes, n);
+        byte[] hostBytesOriginal = hostBytes.clone();
+        LSBEncode(extendedPayload, hostBytes, n, encodeMode==EncodeMode.LSBI ? 4 : 0);
+
+        if(encodeMode == EncodeMode.LSBI)
+            LSBIApply(hostBytesOriginal, hostBytes);
 
         // Write tampered host bytes
 
@@ -68,21 +76,25 @@ public class Steganography {
         // Read host file
 
         FileInputStream is = new FileInputStream(hostFile);
-        byte[] scrBytes = new byte[(int) hostFile.length()];
-        is.read(scrBytes);
+        byte[] hostBytes = new byte[(int) hostFile.length()];
+        is.read(hostBytes);
 
         // Decode payload data with metadata
 
-        byte[] targetBytes = new byte[(int) Math.ceil(hostFile.length() / (8.0 / n))];
-        LSBDecode(scrBytes, targetBytes, n);
+        if(encodeMode == EncodeMode.LSBI)
+            LSBIUnapply(hostBytes);
+
+        int offset = encodeMode==EncodeMode.LSBI ? 4 : 0;
+        byte[] payloadBytes = new byte[(int) Math.ceil((hostFile.length() - offset) / (8.0 / n))];
+        LSBDecode(hostBytes, payloadBytes, n, offset);
 
         // Get payload metadata
 
-        int payloadSize = ByteBuffer.wrap(targetBytes, 0, 4).getInt();
+        int payloadSize = ByteBuffer.wrap(payloadBytes, 0, 4).getInt();
 
         List<Byte> extensionNameBytes = new ArrayList<>();
-        for(int i=4+payloadSize; targetBytes[i] != 0; i++)
-            extensionNameBytes.add(targetBytes[i]);
+        for(int i=4+payloadSize; payloadBytes[i] != 0; i++)
+            extensionNameBytes.add(payloadBytes[i]);
         String extensionName = new String(ArrayUtils.toPrimitive(extensionNameBytes.toArray(new Byte[0])), StandardCharsets.UTF_8);
 
         // Write payload data into output file
@@ -91,12 +103,12 @@ public class Steganography {
         File outputFile = new File(filename);
         FileOutputStream os = new FileOutputStream(outputFile);
 
-        os.write(targetBytes, 4, payloadSize);
+        os.write(payloadBytes, 4, payloadSize);
     }
 
 
-    static void LSBEncode(byte[] payload, byte[] host, int n) {
-        int payloadBit=0, hostBit=8-n;
+    static void LSBEncode(byte[] payload, byte[] host, int n, int hostOffset) {
+        int payloadBit=0, hostBit=8-n + hostOffset*8;
         while(payloadBit < payload.length * 8) {
             for(int i=hostBit; i<hostBit+n; i++)
                 copyBit(payload, payloadBit++, host, i);
@@ -105,14 +117,78 @@ public class Steganography {
     }
 
 
-    static void LSBDecode(byte[] host, byte[] payload, int n) {
-        int payloadBit=0, hostBit=8-n;
+    static void LSBDecode(byte[] host, byte[] payload, int n, int hostOffset) {
+        int payloadBit=0, hostBit=8-n + hostOffset*8;
         while(payloadBit < payload.length * 8) {
             for(int i=hostBit; i<hostBit+n; i++)
                 copyBit(host, i, payload, payloadBit++);
             hostBit += 8;
         }
     }
+
+
+    static void LSBIApply(byte[] original_host, byte[] tampered_host) {
+        int[] changes = new int[4];
+        int[] conservations = new int[4];
+
+        // Count the amount of changes and conservations for each 2-bit group
+
+        for(int i=4; i<original_host.length; i++) {
+            int group = (original_host[i] & 0b00000110) >> 1;
+
+            if(original_host[i] != tampered_host[i])
+                changes[group] += 1;
+            else
+                conservations[group] += 1;
+        }
+
+        // Flip the LSB of the 2-bit groups with more changes than conservations
+
+        boolean[] flipped_groups = new boolean[]{false, false, false, false};
+
+        for(int i=4; i<original_host.length; i++) {
+            int group = (original_host[i] & 0b00000110) >> 1;
+
+            if(changes[group] > conservations[group]) {
+                flipped_groups[group] = true;
+                if((tampered_host[i] & 1) == 1)
+                    tampered_host[i] &= 0b11111110;
+                else
+                    tampered_host[i] |= 0b00000001;
+            }
+        }
+
+        // Create and return the flip byte
+
+        for(int i=0; i<4; i++)
+            if(flipped_groups[i])
+                tampered_host[i] |= 1;
+    }
+
+
+    static void LSBIUnapply(byte[] host) {
+        boolean[] flipped_groups = new boolean[]{false, false, false, false};
+
+        // Recover flipped groups information
+
+        for(int i=0; i<4; i++)
+            if((host[i] & 1) == 1)
+                flipped_groups[i] = true;
+
+        // Restore the flipped groups
+
+        for(int i=4; i<host.length; i++) {
+            int group = (host[i] & 0b00000110) >> 1;
+
+            if(flipped_groups[group]) {
+                if((host[i] & 1) == 1)
+                    host[i] &= 0b11111110;
+                else
+                    host[i] |= 0b00000001;
+            }
+        }
+    }
+
 
     static void copyBit(byte[] src, int srcBitN, byte[] target, int targetBitN) {
         int mask = 1 << (7 - srcBitN % 8);
@@ -124,22 +200,26 @@ public class Steganography {
             target[targetBitN / 8] &= ~(1 << (7 - targetBitN % 8));
     }
 
+
     public static void main(String[] args) throws IOException {
         File hostFile = new File(FILES_DIR.concat("host.bmp"));
         File payloadFile = new File(FILES_DIR.concat("payload.bin"));
-        embed(hostFile, payloadFile, "host_tampered.bmp", EncodeMode.LSB4);
+        embed(hostFile, payloadFile, "host_tampered.bmp", EncodeMode.LSBI);
 
         // TODO: Mostrar error si el tamanio del host es insuficiente
         // TODO: Mostrar error si el host posee compresion
         // TODO: Agregar parsing de argumentos y valores default
 
         File hostTamperedFile = new File(FILES_DIR.concat("host_tampered.bmp"));
-        extract(hostTamperedFile, "payload_recovered", EncodeMode.LSB4);
+        extract(hostTamperedFile, "payload_recovered", EncodeMode.LSBI);
     }
 
     // HT: FF x31 FF  FF x8 FF FF FF FF
     // PL: 0  x31 1   0  x8 0  0  1  0
     // OT: FE x31 FF  FE x8 FE FE FF FE
+
+    // 110 -> 111 F
+    // 111 -> 110 E
 
     public enum EncodeMode {LSB1, LSB4, LSBI}
 
