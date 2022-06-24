d5c24113686b3c9e59d01968c90ddbcbe13852c6
Juan Ignacio Sackmann Sala <jsackmann@itba.edu.ar>
2022-06-23 21:59:12 -0300

    Cryptography working fully

--- TP1/src/main/java/Cryptography.java
+++ TP1/src/main/java/Cryptography.java
@@ -1,4 +1,6 @@
 
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
 import java.nio.charset.StandardCharsets;
 import java.security.*;
 import javax.crypto.*;
@@ -11,7 +13,6 @@ import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.KeySpec;
 import java.util.Arrays;
-import java.util.Base64;
 
 public class Cryptography {
     private static final String SALT = "ssshhhhhhhhhhh!!!!";
@@ -28,19 +29,48 @@ public class Cryptography {
     public static void main(String[] args) throws Exception {
 //        encryptDES(,"blah","password","test".getBytes());
 
-        String chaining="ECB";
-        System.out.println(Arrays.toString("this is the test string".getBytes()));
-//        byte[] cypher=encryptDES(chaining,"pass","this is the test string".getBytes());
-//        System.out.println(Arrays.toString(cypher));
-//        System.out.println(Arrays.toString(decryptDES(chaining,"pass", cypher)));
-        int keyLen=256/8;
 
-//        byte[] cypher=encryptAESECB("pass","this is the test string".getBytes(),keyLen);
-//        System.out.println(Arrays.toString(cypher));
-//        System.out.println(Arrays.toString(decryptAESECB("pass", cypher,keyLen)));
-        byte[] cypher=encryptAES(chaining,"pass","this is the test string".getBytes(),keyLen);
+//        Security.setProperty("crypto.policy", "unlimited");
+//        int maxKeySize = javax.crypto.Cipher.getMaxAllowedKeyLength("AES");
+//        System.out.println("Max Key Size for AES : " + maxKeySize);
+//        Security.addProvider(new BouncyCastleProvider());
+
+        Steganography.EncryptionChaining chaining=Steganography.EncryptionChaining.CFB;
+        Steganography.EncryptionCypher encryptionCypher= Steganography.EncryptionCypher.AES128;
+
+        System.out.println(Arrays.toString("this is the test string".getBytes()));
+
+
+
+
+        byte[] cypher=encrypt(chaining, encryptionCypher,"pass","this is the test string".getBytes());
         System.out.println(Arrays.toString(cypher));
-        System.out.println(Arrays.toString(decryptAES(chaining,"pass", cypher,keyLen)));
+        System.out.println(Arrays.toString(decrypt(chaining, encryptionCypher,"pass",cypher)));
+
+    }
+
+    public static byte[] encrypt(
+    Steganography.EncryptionChaining chaining,Steganography.EncryptionCypher cypher,String password,byte[] content) throws IllegalBlockSizeException, NoSuchPaddingException, UnsupportedEncodingException, NoSuchAlgorithmException, BadPaddingException, InvalidKeySpecException, InvalidKeyException {
+        if (cypher== Steganography.EncryptionCypher.DES){
+            return encryptDES(chaining.toString(),password,content);
+        }
+        String bits=cypher.toString().substring(3);
+        int keyLen =Integer.decode(bits);
+        keyLen/=8;
+
+        return encryptAES(chaining.toString(),password,content,keyLen);
+
+    }
+    public static byte[] decrypt(
+            Steganography.EncryptionChaining chaining,Steganography.EncryptionCypher cypher,String password,byte[] content) throws IllegalBlockSizeException, NoSuchPaddingException, UnsupportedEncodingException, NoSuchAlgorithmException, BadPaddingException, InvalidKeySpecException, InvalidKeyException {
+        if (cypher== Steganography.EncryptionCypher.DES){
+            return decryptDES(chaining.toString(),password,content);
+        }
+        String bits=cypher.toString().substring(3);
+        int keyLen =Integer.decode(bits);
+        keyLen/=8;
+
+        return decryptAES(chaining.toString(),password,content,keyLen);
 
     }
 
@@ -107,36 +137,36 @@ public class Cryptography {
         }
         return both;
     }
-    public static byte[] encryptDES(String chaining,String password,byte[] content) throws IllegalBlockSizeException, NoSuchPaddingException, UnsupportedEncodingException, NoSuchAlgorithmException, BadPaddingException, InvalidKeySpecException, InvalidKeyException {
+    private static byte[] encryptDES(String chaining,String password,byte[] content) throws IllegalBlockSizeException, NoSuchPaddingException, UnsupportedEncodingException, NoSuchAlgorithmException, BadPaddingException, InvalidKeySpecException, InvalidKeyException {
         switch (chaining.toUpperCase()){
             case "ECB":
                 return encryptDESECB(password,content);
             case "CFB":
-                return encryptDESOther("CFB8",8,password,content);
+                return encryptDESOther("CFB8",16,password,content);
             case "OFB":
-                return encryptDESOther("OFB",8,password,content);
+                return encryptDESOther("OFB",16,password,content);
             case "CBC":
-                return encryptDESOther("CBC",8,password,content);
+                return encryptDESOther("CBC",16,password,content);
             default:
                 throw new UnsupportedEncodingException();
         }
     }
 
-    public static byte[] decryptDES(String chaining,String password,byte[] content) throws IllegalBlockSizeException, NoSuchPaddingException, UnsupportedEncodingException, NoSuchAlgorithmException, BadPaddingException, InvalidKeySpecException, InvalidKeyException {
+    private static byte[] decryptDES(String chaining,String password,byte[] content) throws IllegalBlockSizeException, NoSuchPaddingException, UnsupportedEncodingException, NoSuchAlgorithmException, BadPaddingException, InvalidKeySpecException, InvalidKeyException {
         switch (chaining.toUpperCase()){
             case "ECB":
                 return decryptDESECB(password,content);
             case "CFB":
-                return decryptDESOther("CFB8",8,password,content);
+                return decryptDESOther("CFB8",16,password,content);
             case "OFB":
-                return decryptDESOther("OFB",8,password,content);
+                return decryptDESOther("OFB",16,password,content);
             case "CBC":
-                return decryptDESOther("CBC",8,password,content);
+                return decryptDESOther("CBC",16,password,content);
             default:
                 throw new UnsupportedEncodingException();
         }
     }
-    public static byte[] encryptDESECB(String password,byte[] content) throws NoSuchAlgorithmException, IllegalBlockSizeException, BadPaddingException, NoSuchPaddingException, UnsupportedEncodingException, InvalidKeyException, InvalidKeySpecException {
+    private static byte[] encryptDESECB(String password,byte[] content) throws NoSuchAlgorithmException, IllegalBlockSizeException, BadPaddingException, NoSuchPaddingException, UnsupportedEncodingException, InvalidKeyException, InvalidKeySpecException {
         //Se genera la clave para DES
         SecretKeySpec secretKey = new SecretKeySpec(password.getBytes(), "DES");
 
@@ -150,7 +180,7 @@ public class Cryptography {
         return desCipher.doFinal(content);
     }
 
-    public static byte[] decryptDESECB(String password,byte[] content) throws NoSuchAlgorithmException, IllegalBlockSizeException, BadPaddingException, NoSuchPaddingException, UnsupportedEncodingException, InvalidKeyException, InvalidKeySpecException {
+    private static byte[] decryptDESECB(String password,byte[] content) throws NoSuchAlgorithmException, IllegalBlockSizeException, BadPaddingException, NoSuchPaddingException, UnsupportedEncodingException, InvalidKeyException, InvalidKeySpecException {
         //Se genera la clave para DES
         SecretKeySpec secretKey = new SecretKeySpec(password.getBytes(), "DES");
 
@@ -162,7 +192,7 @@ public class Cryptography {
         return desCipher.doFinal(content);
     }
 
-    public static byte[] encryptDESOther(String chaining,int ivLen,String password,byte[]content){
+    private static byte[] encryptDESOther(String chaining,int ivLen,String password,byte[]content){
         try {
 
             byte[][]keys=EVP_BytesToKey(8,ivLen,MessageDigest.getInstance("SHA256"),null,password.getBytes(),65536);
@@ -177,7 +207,7 @@ public class Cryptography {
         return null;
 
     }
-    public static byte[] decryptDESOther(String chaining,int ivLen,String password,byte[]content){
+    private static byte[] decryptDESOther(String chaining,int ivLen,String password,byte[]content){
         try {
             byte[][]keys=EVP_BytesToKey(8,ivLen,MessageDigest.getInstance("SHA256"),null,password.getBytes(),65536);
             IvParameterSpec ivspec = new IvParameterSpec(keys[1]);
@@ -199,11 +229,11 @@ public class Cryptography {
             case "ECB":
                 return encryptAESECB(password,content,keyLength);
             case "CFB":
-                return encryptAESOther("CFB8",16,password,content,keyLength);
+                return encryptAESOther("CFB",16,password,content,keyLength);
             case "OFB":
-                return encryptAESOther("OFB",128/8,password,content,keyLength);
+                return encryptAESOther("OFB",16,password,content,keyLength);
             case "CBC":
-                return encryptAESOther("CBC",128/8,password,content,keyLength);
+                return encryptAESOther("CBC",16,password,content,keyLength);
             default:
                 throw new UnsupportedEncodingException();
         }
@@ -214,17 +244,17 @@ public class Cryptography {
             case "ECB":
                 return decryptAESECB(password,content,keyLength);
             case "CFB":
-                return decryptAESOther("CFB8",16,password,content,keyLength);
+                return decryptAESOther("CFB",16,password,content,keyLength);
             case "OFB":
-                return decryptAESOther("OFB",128/8,password,content,keyLength);
+                return decryptAESOther("OFB",16,password,content,keyLength);
             case "CBC":
-                return decryptAESOther("CBC",128/8,password,content,keyLength);
+                return decryptAESOther("CBC",16,password,content,keyLength);
             default:
                 throw new UnsupportedEncodingException();
         }
     }
 
-    public static byte[] encryptAESECB(String password,byte[] content,int keyLen) {
+    private static byte[] encryptAESECB(String password,byte[] content,int keyLen) {
         try {
             byte[][]keys=EVP_BytesToKey(keyLen,0,MessageDigest.getInstance("SHA256"),null,password.getBytes(),65536);
             IvParameterSpec ivspec = new IvParameterSpec(keys[1]);
@@ -237,7 +267,7 @@ public class Cryptography {
         }
         return null;
     }
-    public static byte[] decryptAESECB(String password,byte[] content,int keyLen) {
+    private static byte[] decryptAESECB(String password,byte[] content,int keyLen) {
         try {
             byte[][]keys=EVP_BytesToKey(keyLen,0,MessageDigest.getInstance("SHA256"),null,password.getBytes(),65536);
             IvParameterSpec ivspec = new IvParameterSpec(keys[1]);
@@ -251,12 +281,12 @@ public class Cryptography {
         }
         return null;
     }
-    public static byte[] encryptAESOther(String chaining,int ivLen,String password,byte[] content,int keyLen) {
+    private static byte[] encryptAESOther(String chaining,int ivLen,String password,byte[] content,int keyLen) {
         try {
             byte[][]keys=EVP_BytesToKey(keyLen,ivLen,MessageDigest.getInstance("SHA256"),null,password.getBytes(),65536);
             IvParameterSpec ivspec = new IvParameterSpec(keys[1]);
             SecretKeySpec secretKey = new SecretKeySpec(keys[0], "AES");
-            Cipher cipher = Cipher.getInstance("AES/"+chaining+"/PKCS5Padding");
+            Cipher cipher = Cipher.getInstance("AES/"+chaining+"/PKCS5PADDING");
             cipher.init(Cipher.ENCRYPT_MODE, secretKey, ivspec);
             return cipher.doFinal(content);
         } catch (Exception e) {
@@ -264,17 +294,17 @@ public class Cryptography {
         }
         return null;
     }
-    public static byte[] decryptAESOther(String chaining,int ivLen,String password,byte[] content,int keyLen) {
+    private static byte[] decryptAESOther(String chaining,int ivLen,String password,byte[] content,int keyLen) {
         try {
             byte[][]keys=EVP_BytesToKey(keyLen,ivLen,MessageDigest.getInstance("SHA256"),null,password.getBytes(),65536);
             IvParameterSpec ivspec = new IvParameterSpec(keys[1]);
             SecretKeySpec secretKey = new SecretKeySpec(keys[0], "AES");
 
-            Cipher cipher = Cipher.getInstance("AES/"+chaining+"/PKCS5Padding\"");
+            Cipher cipher = Cipher.getInstance("AES/"+chaining+"/PKCS5Padding");
             cipher.init(Cipher.DECRYPT_MODE, secretKey, ivspec);
             return cipher.doFinal(content);
         } catch (Exception e) {
-            System.out.println("Error while encrypting: " + e.toString());
+            System.out.println("Error while decrypting: " + e.toString());
         }
         return null;
     }
